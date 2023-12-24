2de20a5a78ffce8231e21e58d60f59991df426f5
Harshaan <11arshaan@gmail.com>
2023-12-24 00:38:40 -0500

    got rectangles

---
+++ LearnGL/LearnGL/files/triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+
+
+int triangle()
+{
+    
+    return 0;
+}
