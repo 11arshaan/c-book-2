13845e395e2db466adadab5492c7b55670f7f84b
Harshaan <11arshaan@gmail.com>
2023-12-24 00:38:49 -0500

    rectangles

--- LearnGL/LearnGL/main.cpp
+++ LearnGL/LearnGL/main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
-
 // GLFW
 #include <GLFW/glfw3.h>
-
 // Window dimensions
-const GLint WIDTH = 800, HEIGHT = 600;
-
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 //process input
 void processInput(GLFWwindow *window)
@@ -18,9 +14,39 @@ if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 glfwSetWindowShouldClose(window, true);
 }
 
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
 
-// The MAIN function, from here we start the application and run the game loop
-int main( )
+
+const char *fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)";
+
+
+
+void check_success(GLuint shader){
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" <<
+        infoLog << std::endl;
+    }
+    
+}
+
+int main()
 {
     // Init GLFW
     glfwInit( );
@@ -60,6 +86,162 @@ int main( )
     // Define the viewport dimensions
     glViewport( 0, 0, screenWidth, screenHeight );
     
+   
+    
+    float vertices[] = {
+    0.3f, 0.75f, 0.0f, // top right
+    0.3f, -0.75f, 0.0f, // bottom right
+    0.15f, -0.75f, 0.0f, // bottom left
+    0.15f, 0.75f, 0.0f // top left
+    };
+    
+    float vertices2[] = {
+    -0.3f, 0.75f, 0.0f, // top right
+    -0.3f, -0.75f, 0.0f, // bottom right
+    -0.15f, -0.75f, 0.0f, // bottom left
+    -0.15f, 0.75f, 0.0f // top left
+    };
+    
+    float vertices3[] = {
+    -0.5f, 0.1f, 0.0f, // top right
+    -0.5f, -0.1f, 0.0f, // bottom right
+    0.5f, -0.1f, 0.0f, // bottom left
+    0.5f, 0.1f, 0.0f // top left
+    };
+    
+    unsigned int indices[] = { // note that we start from 0!
+    0, 1, 3, // first triangle
+    1, 2, 3 // second triangle
+    };
+    
+    
+
+    //RECTANGLE 1
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    
+    //create and bind VBO to set vertices
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    
+    //create and bind element buffer, which is stored in VAO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    GL_STATIC_DRAW);
+    
+    
+    //set vertex attribute pointers to interpret vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    //unbind VBO and VAO
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    
+    
+    //RECTANGLE 2
+    GLuint VAO2;
+    glGenVertexArrays(1, &VAO2);
+    glBindVertexArray(VAO2);
+    
+    
+    //create and bind VBO to set vertices
+    GLuint VBO2;
+    glGenBuffers(1, &VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+    
+    
+    //create and bind element buffer, which is stored in VAO
+    unsigned int EBO2;
+    glGenBuffers(1, &EBO2);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    GL_STATIC_DRAW);
+    
+    
+    //set vertex attribute pointers to interpret vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    
+    
+    //RECTANGLE 3
+    GLuint VAO3;
+    glGenVertexArrays(1, &VAO3);
+    glBindVertexArray(VAO3);
+    
+    
+    //create and bind VBO to set vertices
+    GLuint VBO3;
+    glGenBuffers(1, &VBO3);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO3);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices3), vertices3, GL_STATIC_DRAW);
+    
+    
+    //create and bind element buffer, which is stored in VAO
+    unsigned int EBO3;
+    glGenBuffers(1, &EBO3);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO3);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    GL_STATIC_DRAW);
+    
+    
+    //set vertex attribute pointers to interpret vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    
+    //unbind VBO and VAO
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    
+
+    
+    //create vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    check_success(vertexShader);
+    
+    //create fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    check_success(fragmentShader);
+
+    //create shader program and attach shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    //remove shaders after creating program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    
+    //activate shader program
+    glUseProgram(shaderProgram);
+
+    
+   
+    
+
     // Game loop
     while ( !glfwWindowShouldClose( window ) )
     {
@@ -72,12 +254,39 @@ int main( )
         glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
         
+        
+        
+        glUseProgram(shaderProgram);
+        
+        //rectangle 1
+//        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); wire frame
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        //rectangle 2
+        glBindVertexArray(VAO2);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        //ractangle 3
+        glBindVertexArray(VAO3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        glBindVertexArray(0);
+        
         // Swap the screen buffers
         glfwSwapBuffers( window );
     }
     
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteVertexArrays( 1, &VAO2 );
+    glDeleteVertexArrays( 1, &VAO3 );
+    glDeleteBuffers( 1, &VBO );
+    
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
+
     
-    return EXIT_SUCCESS;
+    
+    return 0;
 }
